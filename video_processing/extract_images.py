3c1a7ed92b85fc6768cafe7cf42a9daa6aa2e82b
Joel Holmes <jholmes@ecr.ai>
2019-07-12 08:17:55 -0400

    output dir

--- video_processing/extract_images.py
+++ video_processing/extract_images.py
@@ -8,7 +8,7 @@ def extract_images(video_loc, index):
     vidcap.set(cv2.CAP_PROP_POS_FRAMES, index)
     success, image = vidcap.read()
     if success:
-        cv2.imwrite("frame{}.png".format(index), image)
+        return index, image
     else:
         raise Exception('invalid frame')
 
@@ -18,16 +18,24 @@ def get_frame_count(path):
     return int(v.get(cv2.CAP_PROP_FRAME_COUNT))
 
 
+def save_image_file(index, image, dir):
+    cv2.imwrite("{}/frame{}.png".format(dir, index), image)
+    return True
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Process video to create images')
     parser.add_argument('video', type=str, help='Path to video file')
+    parser.add_argument('output', type=str, help='Path to directory to place images')
 
     args = parser.parse_args()
 
     video_path = args.video
+    dir_out = args.output
+
     video = cv2.VideoCapture(video_path)
     frame_count = get_frame_count(video_path)
 
     sc = SparkContext('local', 'video_process_extract_images')
-    count = sc.parallelize(range(0, frame_count)).filter(lambda x: extract_images(video_path, x)).count()
+    count = sc.parallelize(range(0, frame_count)).map(lambda x: extract_images(video_path, x)).filter(lambda x: save_image_file(*x, dir_out)).count()
